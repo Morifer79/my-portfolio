40ccb20b9b7624ce94149ddfb2859281fd5ad5a7
Hennadii Laktionov <morifer13@gmail.com>
2024-03-17 16:34:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -92,10 +92,10 @@ let hill1 = document.getElementById("hill1");
 let hill4 = document.getElementById("hill4");
 let hill5 = document.getElementById("hill5");
 
-if (window.location.pathname === "/index.html") {
+if (window.location.pathname === '/index.html') {
   window.addEventListener("scroll", () => {
     let value = window.scrollY;
-
+  
     text.style.marginTop = value * 2.5 + "px";
     leaf.style.top = value * -1.5 + "px";
     leaf.style.left = value * 1.5 + "px";
