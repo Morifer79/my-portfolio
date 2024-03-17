288b29001a2db9bbbda36be2a69575c0b832758a
Hennadii Laktionov <morifer13@gmail.com>
2024-03-17 16:52:32 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -92,7 +92,7 @@ let hill1 = document.getElementById("hill1");
 let hill4 = document.getElementById("hill4");
 let hill5 = document.getElementById("hill5");
 
-if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+if (window.location.pathname === '/my-portfolio/' || window.location.pathname === '/my-portfolio/index.html') {
   window.addEventListener("scroll", () => {
     let value = window.scrollY;
   
